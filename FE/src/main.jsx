f2ab300a292c4fa4f414c5df44f81e85f9353aac
tu11aa <thanhtuhr01@gmail.com>
2025-01-12 17:18:45 +0700

    +Added ReactQueryDevtool as dev dependency

--- FE/src/main.jsx
+++ FE/src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./contexts/auth/AuthContext";
 import ShopProvider from "./contexts/shop/ShopContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -15,6 +16,9 @@ createRoot(document.getElementById("root")).render(
         <QueryClientProvider client={queryClient}>
           <App />
         </QueryClientProvider>
+        {import.meta.env.MODE === "development" && (
+          <ReactQueryDevtools initialIsOpen={false} />
+        )}
       </ShopProvider>
     </AuthProvider>
   </BrowserRouter>
