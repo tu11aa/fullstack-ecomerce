827ae376a467f09ed64e0c3bd660c32b5c3aae7d
tu11aa <thanhtuhr01@gmail.com>
2025-01-16 21:00:44 +0700

    +Added Product detail page *Modify tailwin config: added color classname to reload product color class

--- FE/tailwind.config.js
+++ FE/tailwind.config.js
@@ -1,3 +1,5 @@
+import { PRODUCT_COLOR_CLASSNAME } from "./libs/colorConstant";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -8,5 +10,6 @@ export default {
       },
     },
   },
+  safelist: [...Object.values(PRODUCT_COLOR_CLASSNAME)],
   plugins: [],
 };
