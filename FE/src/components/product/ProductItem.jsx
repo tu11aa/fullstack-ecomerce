0c941c72788f24a6f8a1f49b9fd1c02e12f1bc2a
tu11aa <thanhtuhr01@gmail.com>
2025-01-19 17:07:49 +0700

    *fixed minor bug: cart and buy button did not stick to the bottom

--- FE/src/components/product/ProductItem.jsx
+++ FE/src/components/product/ProductItem.jsx
@@ -9,7 +9,7 @@ const ProductItem = (props) => {
   return (
     <Link
       to={`/products/${data._id}`}
-      className="border border-gray-400 hover:border-black my-4 p-4 text-gray-700 cursor-pointer"
+      className="flex flex-col border border-gray-400 hover:border-black my-4 p-4 text-gray-700 cursor-pointer"
     >
       <div className="overflow-hidden pb-2">
         <img src={data.image[0]} alt={data.name} />
@@ -18,7 +18,7 @@ const ProductItem = (props) => {
       <strong>
         {data.price} {currency}
       </strong>
-      <div className="flex flex-row justify-between">
+      <div className="flex flex-row justify-between mt-auto">
         <button>
           <svg
             xmlns="http://www.w3.org/2000/svg"
