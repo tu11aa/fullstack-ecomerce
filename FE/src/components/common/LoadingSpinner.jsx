0ae11a2825dde02a7c98f4f180eae9e2e4429735
tu11aa <thanhtuhr01@gmail.com>
2025-02-12 01:00:24 +0700

    *Reduce LoadingSpinner height

--- FE/src/components/common/LoadingSpinner.jsx
+++ FE/src/components/common/LoadingSpinner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LoadingSpinner = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex items-center justify-center min-h-32">
       <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900" />
     </div>
   );
