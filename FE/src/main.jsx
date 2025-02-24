b099bf4e8126c7db1b68432b54694b96c23300a3
tu11aa <thanhtuhr01@gmail.com>
2025-02-24 10:18:10 +0700

    +added base url

--- FE/src/main.jsx
+++ FE/src/main.jsx
@@ -10,7 +10,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <ShopProvider>
