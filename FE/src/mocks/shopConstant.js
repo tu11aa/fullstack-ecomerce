b3227dd4e0bedb4b5680a24e9a6d35f4b19b385c
tu11aa <thanhtuhr01@gmail.com>
2025-03-07 17:28:27 +0700

    +Added mock data for demo deploy

---
+++ FE/src/mocks/shopConstant.js
@@ -0,0 +1,23 @@
+export const SHOP_CONSTANT = {
+  shop_categories: {
+    //temporary
+    gender: {
+      name: "category",
+      displayName: "GENDER",
+      fields: [
+        { displayName: "MEN", value: "men" },
+        { displayName: "WOMEN", value: "women" },
+        { displayName: "KIDS", value: "kids" },
+      ],
+    },
+    category: {
+      name: "subCategory",
+      displayName: "CATEGORY",
+      fields: [
+        { displayName: "TOPWEAR", value: "topwear" },
+        { displayName: "BOTTOMWEAR", value: "bottomwear" },
+        { displayName: "FOOTWEAR", value: "footwear" },
+      ],
+    },
+  },
+};
