8b4ea2bdc5f19c325a0c57578e01c4890f6997f9
tu11aa <thanhtuhr01@gmail.com>
2025-03-08 12:28:46 +0700

    +added allowed origin

--- BE/server.js
+++ BE/server.js
@@ -1,18 +1,20 @@
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
-import connectDB from "./config/mongodb.js";
-import configCloudinary from "./config/cloudinary.js";
-import productRouter from "./routes/productRoute.js";
-import userRouter from "./routes/userRoute.js";
-import cartRouter from "./routes/cartRoute.js";
-import errorMiddleware from "./middlewares/errorMiddleware.js";
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import connectDB from './config/mongodb.js';
+import configCloudinary from './config/cloudinary.js';
+import authRouter from './routes/authRoute.js';
+import productRouter from './routes/productRoute.js';
+import userRouter from './routes/userRoute.js';
+import cartRouter from './routes/cartRoute.js';
+import errorMiddleware from './middlewares/errorMiddleware.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 connectDB();
 configCloudinary();
 
@@ -26,6 +28,7 @@ app.use(
         ? [
             'http://localhost:8080',
             'http://localhost:4173',
+            'https://fullstack-ecomerce.onrender.com',
           ]
         : process.env.DOMAIN,
     credentials: true,
@@ -38,11 +41,12 @@ app.use(
   })
 );
 
-app.get("/test", (_, res) => res.status(200).send("Hello world"));
+app.get('/test', (_, res) => res.status(200).send('Hello world'));
 
-app.use("/api/v1/products", productRouter);
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/carts", cartRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/products', productRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/carts', cartRouter);
 
 app.use(errorMiddleware);
 
